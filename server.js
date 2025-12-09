45ac88f3d654dfa379cd00b1490de784402a5a6d
SahasOP <sahasprajapati54@gmail.com>
2025-12-10 03:21:11 +0530

    testing

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 import dotenv from "dotenv";
 import cloudinary from "cloudinary";
-import dbConnection from "./Config/dbConnection.js";
 import app from "./app.js";
+import dbConnection from "./Config/dbConnection.js";
 
 dotenv.config();
 
-// Database connection (connect once)
-await dbConnection();
+dbConnection();
 
-// Cloudinary config
 cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
