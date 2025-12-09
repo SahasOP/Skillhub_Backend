d7f60b64cb442086e94daccaff8c67670abe13bc
SahasOP <sahasprajapati54@gmail.com>
2025-12-10 03:14:10 +0530

    fix

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cloudinary from 'cloudinary';
-import dbConnection from './Config/dbConnection.js';
-import app from './app.js';
+import dotenv from "dotenv";
+import cloudinary from "cloudinary";
+import dbConnection from "./Config/dbConnection.js";
+import app from "./app.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5034;
+// Database connection (connect once)
+await dbConnection();
 
+// Cloudinary config
 cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.listen(PORT, () => {
-    dbConnection();
-    console.log(`Server running at http://localhost:${PORT}`);
-});
+export default app;
