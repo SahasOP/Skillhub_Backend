8e24935a13aefdd1bfeba68a181f26807eb9204a
SahasOP <sahasprajapati54@gmail.com>
2025-12-10 03:33:23 +0530

    test

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import cors from 'cors'
 const app = express();
 
 app.use(express.json())
-app.use(morgan('dev'))
+// app.use(morgan('dev'))
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
 
@@ -18,11 +18,11 @@ app.use(cors({
     origin: "*"
 }));
 
-app.use('/api/v1/user', userRoutes)
-app.use('/api/v1/test', testRoutes)
-app.use('/api/v1', topicRoutes)
+// app.use('/api/v1/user', userRoutes)
+// app.use('/api/v1/test', testRoutes)
+// app.use('/api/v1', topicRoutes)
 
-app.use(errorMiddleware)
+// app.use(errorMiddleware)
 app.get("/", (req, res) => {
     res.send("SkillHub Backend Running ✅");
 });
