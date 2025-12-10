ec2fbfb9bc78ca6cba1554727b2a115bb3c78bb7
SahasOP <sahasprajapati54@gmail.com>
2025-12-10 22:52:51 +0530

    test

---
+++ api/index.js
@@ -0,0 +1,4 @@
+import "../server.js"; // boots env, db, cloudinary
+import app from "../app.js";
+
+export default app;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import dotenv from "dotenv";
 import cloudinary from "cloudinary";
-import app from "./app.js";
 import dbConnection from "./Config/dbConnection.js";
 
 dotenv.config();
@@ -8,9 +7,7 @@ dotenv.config();
 dbConnection();
 
 cloudinary.v2.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
-export default app;
